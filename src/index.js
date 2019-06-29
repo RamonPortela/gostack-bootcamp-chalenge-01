3e7de760b988adac87ecba11cb342d97a6ddc64c
Ramon Portela Santos <portela.ramon96@gmail.com>
2019-06-28 22:46:46 -0300

    id validation and requests counter middlewares

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 
+let counter = 0;
 const projects = [];
 
 const app = express();
 
 app.use(express.json());
+app.use((req, res, next) => {
+  counter += 1;
+  console.log(`Requests counter: ${counter}`);
+
+  next();
+});
+
+function validateIdExistenceMiddleware(req, res, next) {
+  const id = parseInt(req.params.id, 10);
+
+  if (!projects.find(project => project.id === id))
+    return res.status(400).json({ error: 'No projects found with this id' });
+
+  return next();
+}
 
 app.get('/projects', (req, res) => {
   return res.status(200).json({ projects });
@@ -13,16 +29,23 @@ app.get('/projects', (req, res) => {
 app.post('/projects', (req, res) => {
   const { id, title } = req.body;
 
-  projects.push({
+  if (projects.find(project => project.id === id))
+    return res
+      .status(400)
+      .json({ error: 'Project with this id already exists' });
+
+  const project = {
     id,
     title,
     tasks: [],
-  });
+  };
 
-  return res.status(201).send();
+  projects.push(project);
+
+  return res.status(201).json(project);
 });
 
-app.put('/projects/:id', (req, res) => {
+app.put('/projects/:id', validateIdExistenceMiddleware, (req, res) => {
   const id = parseInt(req.params.id, 10);
   const { title } = req.body;
 
@@ -33,7 +56,7 @@ app.put('/projects/:id', (req, res) => {
   return res.status(204).send();
 });
 
-app.delete('/projects/:id', (req, res) => {
+app.delete('/projects/:id', validateIdExistenceMiddleware, (req, res) => {
   const id = parseInt(req.params.id, 10);
 
   const projectIndex = projects.findIndex(p => p.id === id);
@@ -41,6 +64,17 @@ app.delete('/projects/:id', (req, res) => {
   projects.splice(projectIndex, 1);
 
   return res.status(204).send();
+});
+
+app.post('/projects/:id/tasks', validateIdExistenceMiddleware, (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id === id);
+
+  project.tasks.push(title);
+
+  return res.status(201).json(project);
 });
 
 app.listen(3333);
