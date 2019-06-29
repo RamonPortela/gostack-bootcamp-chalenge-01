726529c5fb58951c4f834d8c0f3ee28d1785b01c
Ramon Portela Santos <portela.ramon96@gmail.com>
2019-06-28 22:16:32 -0300

    projects route get and post

--- src/index.js
+++ src/index.js
@@ -6,4 +6,20 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/projects', (req, res) => {
+  return res.status(200).json({ projects });
+});
+
+app.post('/projects', (req, res) => {
+  const { id, title } = req.body;
+
+  projects.push({
+    id,
+    title,
+    tasks: [],
+  });
+
+  res.status(201).send();
+});
+
 app.listen(3333);
